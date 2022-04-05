bdf7e5b4d5f549ed9397873f1bb6a2a0e369e712
Kennedy Silva <kennedy2000silva@hotmail.com>
2022-04-05 12:46:22 -0300

    refact: fragment

---
+++ src/modules/pokemon/fragments/pokemon-list/pokemon-list.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useMemo, useState } from "react";
+import { Autocomplete, Box, Grid, TextField } from "@mui/material";
+import { PokemonCard } from "modules/pokemon/cards/pokemon-card/pokemon-card";
+import { PokemonEntity } from "types/entities";
+import { useInView } from "react-intersection-observer";
+import { useGet } from "zoe-data";
+
+interface PokemonListProps {}
+
+export function PokemonList(props: PokemonListProps) {
+  const [page, setPage] = useState<number>(0);
+  const [search, setSearch] = useState<string>("");
+
+  const { data } = useGet<PokemonEntity[]>("/pokemon");
+  const { ref, inView } = useInView({
+    threshold: 0,
+  });
+
+  // fiz paginacao no front e nao no back
+  const paginatedPokemons = useMemo(() => {
+    if (!data) {
+      return [];
+    }
+    if (search && search !== "") {
+      return data.filter((pokemon) =>
+        pokemon.name.toLowerCase().includes(search.toLowerCase())
+      );
+    }
+
+    return data.slice(0, page * 21);
+  }, [data, page, search]);
+
+  useEffect(() => {
+    setPage(page + 1);
+  }, [inView]);
+
+  const handleSearch = (value: string | PokemonEntity | null) => {
+    if (value && (value as PokemonEntity)?.name) {
+      setSearch((value as PokemonEntity).name);
+      return;
+    }
+    setSearch(value as string);
+  };
+
+  return (
+    <>
+      <Grid item container xs={12} justifyContent={"center"} sx={{ mb: 4 }}>
+        <Grid item xs={6}>
+          <Autocomplete
+            renderInput={(props) => (
+              <TextField {...props} label={"Search pokemons"} />
+            )}
+            options={data || []}
+            freeSolo
+            getOptionLabel={(option) => option.name || search || ""}
+            onChange={(_, value, reason) =>
+              reason === "clear" ? handleSearch("") : handleSearch(value)
+            }
+          />
+        </Grid>
+      </Grid>
+      {paginatedPokemons?.map((pokemon, index) => (
+        <Grid item xs={12} md={4} key={index}>
+          <PokemonCard pokemon={pokemon} />
+        </Grid>
+      ))}
+      <Box ref={ref} mx={5} my={5} />
+    </>
+  );
+}
--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -4,69 +4,16 @@ import { useGet } from "zoe-data";
 import { PokemonEntity } from "types/entities";
 import { PokemonCard } from "modules/pokemon/cards/pokemon-card/pokemon-card";
 import { useInView } from "react-intersection-observer";
+import { PokemonList } from "modules/pokemon/fragments/pokemon-list";
 
 export function Home() {
-  const [page, setPage] = useState<number>(0);
-  const [search, setSearch] = useState<string>("");
-
-  const { data } = useGet<PokemonEntity[]>("/pokemon");
-  const { ref, inView } = useInView({
-    threshold: 0,
-  });
-
-  // fiz paginacao no front e nao no back
-  const paginatedPokemons = useMemo(() => {
-    if (!data) {
-      return [];
-    }
-    if (search && search !== "") {
-      return data.filter((pokemon) =>
-        pokemon.name.toLowerCase().includes(search.toLowerCase())
-      );
-    }
-
-    return data.slice(0, page * 21);
-  }, [data, page, search]);
-
-  useEffect(() => {
-    setPage(page + 1);
-  }, [inView]);
-
-  const handleSearch = (value: string | PokemonEntity | null) => {
-    if (value && (value as PokemonEntity)?.name) {
-      setSearch((value as PokemonEntity).name);
-      return;
-    }
-    setSearch(value as string);
-  };
-
   return (
     <Container>
       <Grid container spacing={2}>
         <Grid item xs={12} display={"flex"} justifyContent={"center"}>
           <img src={"https://crisgon.github.io/pokedex/src/images/logo.png"} />
         </Grid>
-        <Grid item container xs={12} justifyContent={"center"}>
-          <Grid item xs={6}>
-            <Autocomplete
-              renderInput={(props) => (
-                <TextField {...props} label={"Pokemon"} />
-              )}
-              options={data || []}
-              freeSolo
-              getOptionLabel={(option) => option.name || search || ""}
-              onChange={(_, value, reason) =>
-                reason === "clear" ? handleSearch("") : handleSearch(value)
-              }
-            />
-          </Grid>
-        </Grid>
-        {paginatedPokemons?.map((pokemon, index) => (
-          <Grid item xs={12} md={4} key={index}>
-            <PokemonCard pokemon={pokemon} />
-          </Grid>
-        ))}
-        <Box ref={ref} mx={5} my={5} />
+        <PokemonList />
       </Grid>
     </Container>
   );
